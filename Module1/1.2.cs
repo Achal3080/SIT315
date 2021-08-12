8d6b00089176b125a52287f3672c9008110c3342
Achal3080 <63145578+Achal3080@users.noreply.github.com>
2021-08-12 18:56:32 +1000

    Add files via upload

---
+++ Module1/1.2.cs
@@ -0,0 +1,34 @@
+﻿const uint8_t LedPin = 13;
+const uint8_t PIR = 
+    2;
+
+uint8_t PIRstate = LOW;
+
+void setup()
+{
+    pinMode(LedPin, OUTPUT);
+    pinMode(PIR, INPUT);
+
+    Serial.begin(9600);
+
+    attachInterrupt(digitalPinToInterrupt(PIR), MotionDetected, CHANGE);
+}
+
+void loop()
+{
+}
+
+void MotionDetected()
+{
+    PIRstate = digitalRead(PIR);
+    if (PIRstate)
+    {
+        digitalWrite(LedPin, HIGH);
+        Serial.println("Motion Has Been Detected");
+    }
+    else
+    {
+        digitalWrite(LedPin, LOW);
+        Serial.println("There is no Motion Detected");
+    }
+}
